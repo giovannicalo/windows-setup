86f0a83bce9264033400d00a74585b2e96d4cdba
Giovanni Calò <giovanni.calo@live.it>
2023-05-04 03:10:23 +0100

    Added unnecessary services list

---
+++ source/services.ps1
@@ -0,0 +1,14 @@
+$services = @(
+	# Connected User Experiences and Telemetry [TODO: Verify]
+	"DiagTrack",
+	# Windows Error Reporting Service [TODO: Verify]
+	"WerSvc",
+	# Xbox Live Auth Manager [TODO: Verify]
+	"XblAuthManager",
+	# Xbox Live Game Save [TODO: Verify]
+	"XblGameSave",
+	# Xbox Accessory Management Service [TODO: Verify]
+	"XboxGipSvc",
+	# Xbox Live Networking Service [TODO: Verify]
+	"XboxNetApiSvc"
+)
