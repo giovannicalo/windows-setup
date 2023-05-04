7a31f38c74c435dcbfe8f77b52a198b160981736
Giovanni Calò <giovanni.calo@live.it>
2023-05-04 03:17:15 +0100

    Added footer module

---
+++ source/display-footer.ps1
@@ -0,0 +1,11 @@
+function displayFooter {
+	Write-Host
+	Write-Host "------------------------------------------------------------------------"
+	Write-Host "|                          " -NoNewline
+	Write-Host ">>> [ALL DONE] <<<" -ForegroundColor "Green" -NoNewline
+	Write-Host "                          |"
+	Write-Host "------------------------------------------------------------------------"
+	Write-Host
+	Write-Host "You should reboot."
+	Write-Host
+}
