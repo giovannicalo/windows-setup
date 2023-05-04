2fe53e79648d75487389a2305b022d1d840ca54f
Giovanni Calò <giovanni.calo@live.it>
2023-05-04 03:09:57 +0100

    Added unnecessary scheduled tasks list

---
+++ source/tasks.ps1
@@ -0,0 +1,26 @@
+$tasks = @(
+	# Microsoft Customer Experience Improvement Program [TODO: Verify]
+	"Consolidator",
+	# Feedback [TODO: Verify]
+	"DmClient",
+	# Feedback [TODO: Verify]
+	"DmClientOnScenarioDownload",
+	# Microsoft Compatibility Telemetry [TODO: Verify]
+	"Microsoft Compatibility Appraiser",
+	# Microsoft Customer Experience Improvement Program [TODO: Verify]
+	"Microsoft-Windows-DiskDiagnosticDataCollector",
+	# Microsoft Customer Experience Improvement Program [TODO: Verify]
+	"Microsoft-Windows-DiskDiagnosticResolver",
+	# Microsoft Compatibility Telemetry [TODO: Verify]
+	"ProgramDataUpdater",
+	# Windows Error Reporting [TODO: Verify]
+	"QueueReporting",
+	# Microsoft Customer Experience Improvement Program [TODO: Verify]
+	"Proxy",
+	# Microsoft Customer Experience Improvement Program [TODO: Verify]
+	"UsbCeip",
+	# Xbox Live Game Save [TODO: Verify]
+	"XblGameSaveTask",
+	# Xbox Live Game Save [TODO: Verify]
+	"XblGameSaveTaskLogon"
+)
