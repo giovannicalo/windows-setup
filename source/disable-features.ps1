de18e63592b51d45e23624b9bbdeba4d9ed21fed
Giovanni Calò <giovanni.calo@live.it>
2023-05-04 04:14:33 +0100

    Suppressed warning

--- source/disable-features.ps1
+++ source/disable-features.ps1
@@ -3,6 +3,6 @@ function disableFeatures {
 	$features = $args[0]
 	$null = Get-WindowsOptionalFeature -Online | Where-Object {
 		$_.FeatureName -in $features -and $_.State -eq "Enabled"
-	} | Disable-WindowsOptionalFeature -NoRestart -Online
+	} | Disable-WindowsOptionalFeature -NoRestart -Online -WarningAction SilentlyContinue
 	Write-Host " [DONE]" -ForegroundColor "Green"
 }
