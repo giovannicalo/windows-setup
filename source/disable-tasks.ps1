772410d9911e94ed52259c98afea2d598754c7ff
Giovanni Calò <giovanni.calo@live.it>
2023-05-04 03:14:54 +0100

    Added task disabling module

---
+++ source/disable-tasks.ps1
@@ -0,0 +1,8 @@
+function disableTasks {
+	Write-Host "Disabling scheduled tasks..." -NoNewline
+	$tasks = $args[0]
+	$null = Get-ScheduledTask | Where-Object {
+		$_.TaskName -in $tasks -and $_.State -eq "Ready"
+	} | Disable-ScheduledTask
+	Write-Host " [DONE]" -ForegroundColor "Green"
+}
