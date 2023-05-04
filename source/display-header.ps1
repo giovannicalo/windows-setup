8d4afefa8943e5e00b7404d748e47026f32d5e95
Giovanni Calò <giovanni.calo@live.it>
2023-05-04 03:17:05 +0100

    Added header module

---
+++ source/display-header.ps1
@@ -0,0 +1,9 @@
+function displayHeader {
+	Write-Host
+	Write-Host "------------------------------------------------------------------------"
+	Write-Host "|                           " -NoNewline
+	Write-Host "Windows 10 Setup" -ForegroundColor "Green" -NoNewline
+	Write-Host "                           |"
+	Write-Host "------------------------------------------------------------------------"
+	Write-Host
+}
