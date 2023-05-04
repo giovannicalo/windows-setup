039ef977cf2cfa5a3cd4016f8f4607ddff3d0075
Giovanni Calò <giovanni.calo@live.it>
2023-05-04 03:08:44 +0100

    Added unnecessary store apps list

---
+++ source/apps.ps1
@@ -0,0 +1,72 @@
+$apps = @(
+	# 3D Builder
+	"Microsoft.3DBuilder",
+	# Cortana
+	"Microsoft.549981C3F5F10",
+	# [TODO: Verify]
+	"Microsoft.Advertising.Xaml",
+	# Weather
+	"Microsoft.BingWeather",
+	# Get Help
+	"Microsoft.GetHelp",
+	# Tips
+	"Microsoft.Getstarted",
+	# Paint 3D
+	"Microsoft.MSPaint",
+	# 3D Viewer
+	"Microsoft.Microsoft3DViewer",
+	# Microsoft 365 (Office)
+	"Microsoft.MicrosoftOfficeHub",
+	# Solitaire & Casual Games
+	"Microsoft.MicrosoftSolitaireCollection",
+	# Sticky Notes
+	"Microsoft.MicrosoftStickyNotes",
+	# Mixed Reality Portal
+	"Microsoft.MixedReality.Portal",
+	# OneNote for Windows 10
+	"Microsoft.Office.OneNote",
+	# [TODO: Verify]
+	"Microsoft.OneConnect",
+	# People
+	"Microsoft.People",
+	# Print 3D
+	"Microsoft.Print3D",
+	# [TODO: Verify]
+	"Microsoft.Services.Store.Engagement",
+	# Skype
+	"Microsoft.SkypeApp",
+	# [TODO: Verify]
+	"Microsoft.StorePurchaseApp",
+	# [TODO: Verify]
+	"Microsoft.Wallet",
+	# Alarms & Clock
+	"Microsoft.WindowsAlarms",
+	# Camera
+	"Microsoft.WindowsCamera",
+	# Calendar, Mail
+	"Microsoft.WindowsCommunicationsApps"
+	# Feedback Hub
+	"Microsoft.WindowsFeedbackHub",
+	# Maps
+	"Microsoft.WindowsMaps",
+	# Voice Recorder
+	"Microsoft.WindowsSoundRecorder",
+	# Xbox Console Companion
+	"Microsoft.XboxApp",
+	# [TODO: Verify]
+	"Microsoft.Xbox.TCUI",
+	# [TODO: Verify]
+	"Microsoft.XboxGameOverlay",
+	# Xbox Game Bar
+	"Microsoft.XboxGamingOverlay",
+	# [TODO: Verify]
+	"Microsoft.XboxIdentityProvider",
+	# [TODO: Verify]
+	"Microsoft.XboxSpeechToTextOverlay",
+	# Phone Link
+	"Microsoft.YourPhone",
+	# Groove Music
+	"Microsoft.ZuneMusic",
+	# Movies & TV
+	"Microsoft.ZuneVideo"
+)
