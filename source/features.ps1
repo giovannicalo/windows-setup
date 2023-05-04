839be9e3d72b889ef372dcddf7abdc3c163bc222
Giovanni Calò <giovanni.calo@live.it>
2023-05-04 03:09:25 +0100

    Added unnecessary optional features list

---
+++ source/features.ps1
@@ -0,0 +1,12 @@
+$features = @(
+	# Internet Explorer 11 [TODO: Verify]
+	"Internet-Explorer-Optional-amd64",
+	# PowerShell 2 [TODO: Verify]
+	"MicrosoftWindowsPowerShellV2",
+	# PowerShell 2 [TODO: Verify]
+	"MicrosoftWindowsPowerShellV2Root",
+	# XPS Viewer [TODO: Verify]
+	"Printing-XPSServices-Features",
+	# Windows Media Player [TODO: Verify]
+	"WindowsMediaPlayer"
+)
