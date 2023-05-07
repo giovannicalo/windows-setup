fcf948d927ed5fd8796b7dd6740d124906574788
Giovanni Calò <giovanni.calo@live.it>
2023-05-07 23:52:47 +0100

    Added bin and lib folders

--- source/folders.ps1
+++ source/folders.ps1
@@ -3,8 +3,12 @@ $folders = @(
 	@("$Env:LocalAppData\Microsoft\OneDrive", "UNSET"),
 	# Remove OneDrive leftovers
 	@("$Env:ProgramData\Microsoft OneDrive", "UNSET"),
-	# Create development folder
-	@("$Env:UserProfile\Development", "SET"),
+	# Create bin folder
+	@("$Env:UserProfile\bin", "SET"),
+	# Create dev folder
+	@("$Env:UserProfile\dev", "SET"),
+	# Create lib folder
+	@("$Env:UserProfile\lib", "SET"),
 	# Remove OneDrive leftovers
 	@("$Env:UserProfile\OneDrive", "UNSET")
 )
