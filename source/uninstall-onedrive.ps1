d6656fafd7b4c71b188a9f63a560a6128cbdf58d
Giovanni Calò <giovanni.calo@live.it>
2023-05-09 01:22:59 +0100

    Mitigated an issue with removing OneDrive folders

--- source/uninstall-onedrive.ps1
+++ source/uninstall-onedrive.ps1
@@ -3,6 +3,7 @@ function uninstallOneDrive {
 	Write-Host "Uninstalling OneDrive..." -NoNewline
 	Stop-Process -ErrorAction SilentlyContinue -Name OneDrive
 	Start-Process -ArgumentList "/uninstall" -FilePath "$Env:SystemRoot\SysWOW64\OneDriveSetup.exe" -Wait
+	Start-Sleep 5
 	Write-Host " [DONE]" -ForegroundColor "Green"
 	Write-Host
 }
